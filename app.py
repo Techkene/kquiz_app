8bb8dc6f3e0a5f3a776ef838e4ae003fa838bd6b
kenechukwu Anoliefo <keneanoliefo24@gmail.com>
2024-03-22 12:01:41 +0100

    Added more templates also added their individual route

--- app.py
+++ app.py
@@ -16,5 +16,26 @@ def home():
 def login():
     return render_template("login.html")
 
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template("contact_us.html")
+
+@app.route('/set_question')
+def set_question():
+    return render_template("set_question.html")
+
+@app.route('/answer_question')
+def answer_question():
+    return render_template("answer_question.html")
+
+@app.route('/logout')
+def logout():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug= True)
